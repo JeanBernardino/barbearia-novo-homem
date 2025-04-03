f1dfe0c4932d2c0411fc5396988adb938568096e
Jean Carlos Bernardino <jeancarlosbernardino98@gmail.com>
2025-04-03 19:28:12 -0300

    fix soma vendas

--- src/pages/gestao/relatorios/GestaoRelatorioViewPage.vue
+++ src/pages/gestao/relatorios/GestaoRelatorioViewPage.vue
@@ -296,8 +296,10 @@ const onFilterConfirm = async () => {
       }
 
       const row = groupedData.get(data)!;
+      console.log(venda);
+      console.log(venda.quantidade);
       row.totalProdutos += venda.quantidade;
-      row.produtos[venda.produto_id] = (row.servicos[venda.produto_id] || 0) + venda.quantidade;
+      row.produtos[venda.produto_id] = (row.produtos[venda.produto_id] || 0) + venda.quantidade;
 
       const valorTotalVenda = venda.produto_valor * venda.quantidade;
       row.valorTotalVendas += valorTotalVenda;
